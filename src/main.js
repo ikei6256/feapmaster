85b5c53baa4a1e4bcb3ea0560ebb4d7c7de2dde7
ikei6256-gushi <ryu.e115752@gmail.com>
2020-11-20 12:53:13 +0900

    test push

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrapのスタイルシート側の機能を読み込む
 import "bootstrap/dist/js/bootstrap"; // BootstrapのJavaScript側の機能を読み込む
 
-Vue.config.productionTip = true // 開発中は true にしたほうがいいかも
+Vue.config.productionTip = false; // 開発中は true にしたほうがいいかも
 // true の場合の方が開発者向けのメッセージがコンソールによりたくさん出るようです。
 
 /* eslint-disable no-new */
